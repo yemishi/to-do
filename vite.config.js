dd2cc914beadfd1c4c060922816a2a51542823e5
Davi Damião <daviddperera10@gmail.com>
2023-10-31 17:41:27 +0000

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
+export default {
   build: {
-    outDir: 'dist', // Diretório de saída para a construção
-    assetsDir: 'assets', // Diretório de ativos
+    outDir: 'build',
   },
-});
+  // ... outras configurações
+}
