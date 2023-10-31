96514709fab523c8d95850386b50168813ec91b7
Davi Damião <daviddperera10@gmail.com>
2023-10-31 16:59:59 +0000

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    outDir: 'build',
+  },
+  // ... outras configurações
+}
